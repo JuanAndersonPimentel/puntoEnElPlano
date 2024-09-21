73079dc91916b9ebd460d1d78c0945df83687ed3
UserGPC <UserGPC@DESKTOP-4VVRAQ5>
2024-09-21 15:13:08 -0400

    Agregar archivos de proyecto.

---
+++ puntoEnElPlano/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puntoEnElPlano
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            punto punto = new punto();
+
+            try
+            {
+                Console.WriteLine("ingrese el valor de x:");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("ingrese el valor de y:");
+                int y = int.Parse(Console.ReadLine());
+
+                punto.cargarpunto(x, y);
+
+                punto.imprimirCuadrante();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("error: debe ingresar numeros validos:");
+                
+            }
+        }
+    }
+}
---
+++ puntoEnElPlano/punto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puntoEnElPlano
+{
+    public class punto
+    {
+        private int x;
+        private int y;
+
+        public void cargarpunto(int valorx, int valory)
+        {
+            x = valorx;
+            y = valory;
+   
+        }
+        public void imprimirCuadrante() 
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("el punto esta en el primer cuadrante:");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("el puneto esta en el segundo cuadrante:");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("el punto esta en el cuatro cuadrante:");
+            }
+            else if (x == 0 && y == 0)
+            {
+                Console.WriteLine("el punto esta en el origen:");
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine("el punto esta sobre el eje y.");
+            }
+            else if (y == 0)
+            {
+                Console.WriteLine("el punto esta sobre el eje x.");
+            }
+
+
+            
+        }
+        
+
+        
+
+        
+       
+        
+           
+
+
+
+    }
+
+
+}
